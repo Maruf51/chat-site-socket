4577f5484ed0f43aed9a9822b8ddf8be4f4d0235
Maruf Hossain <hossain815265@gmail.com>
2021-05-18 17:46:00 +0600

    done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");

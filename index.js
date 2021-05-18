e9d7b38c6691a4aea4dfec41d1e4d1fc7b84d92a
Maruf Hossain <hossain815265@gmail.com>
2021-05-18 17:39:28 +0600

    done

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const app = express()
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
+
+
+let users = []
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === socketId) &&
+    users.push({userId, socketId})
+}
+
+const removeUser = (socketId) => {
+  users = users.filter(user => user.socketId !== socketId)
+}
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on('connection', (socket) => {
+  // when connect
+  console.log('a user connected')
+  // adding user to socket server
+  socket.on('addUser', (userId) => {
+    addUser(userId, socket.id)
+    io.emit('getSocketUsers', users)
+  })
+
+  //send and get message
+  socket.on("sendMessage", ({ messageId, sendId, receiverId, message }) => {
+    const user = getUser(receiverId);
+    if (user) {
+      io.to(user.socketId).emit("getMessage", {
+        sendId,
+        messageId,
+        message,
+      });
+    }
+  });
+
+  // when disconnect
+  socket.on('disconnect', () => {
+    console.log('a user disconnected')
+    removeUser(socket.id)
+    io.emit('getSocketUsers', users)
+  })
+})
+
+server.listen(8000, () => {
+      console.log('listening on *:8000');
+    });

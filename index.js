7ad81f1b3a0554ec0f200942545e4d4066936568
Maruf Hossain <hossain815265@gmail.com>
2024-07-16 21:32:39 +0600

    change port

--- index.js
+++ index.js
@@ -86,6 +86,8 @@ io.on('connection', (socket) => {
   })
 })
 
-server.listen(8000, () => {
-      console.log('listening on *:8000');
-    });
+const PORT = process.env.PORT || 8000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
